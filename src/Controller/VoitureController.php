4f0be96297add58455752b60778545c9fff5cbcc
Tom Fontaine <tom.fnt9@gmail.com>
2024-03-21 16:27:43 +0100

    Add specific voiture route path with id set in parameter & add redirect route path in accueil.html.twig and voiture.html.twig

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Voiture;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class VoitureController extends AbstractController
 {
-    #[Route('/voiture', name: 'voiture')]
-    public function index(): Response
+    #[Route('/voiture/{id}', name: 'voiture_index')]
+    public function index(Voiture $voiture): Response
     {
+        if(!$voiture) {
+            return $this->redirectToRoute('app_home');
+        }
+
+
         return $this->render('voiture.html.twig', [
-            'controller_name' => 'VoitureController',
+            'voiture' => $voiture,
         ]);
     }
 }

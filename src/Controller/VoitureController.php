9cf2ad0f3debad667a3fe2c59658db137d52ac18
Tom Fontaine <tom.fnt9@gmail.com>
2024-03-28 14:53:27 +0100

    Implement delete voiture route & fix missing close balise in base.html.twig

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -3,12 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Voiture;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class VoitureController extends AbstractController
 {
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,)
+    {
+
+    }
     #[Route('/voiture/{id}', name: 'voiture_index')]
     public function index(Voiture $voiture): Response
     {
@@ -20,5 +28,31 @@ class VoitureController extends AbstractController
         return $this->render('voiture.html.twig', [
             'voiture' => $voiture,
         ]);
+    }
+
+    #[Route('/voiture/{id}/delete', name: 'voiture_delete')]
+    public function supprimerVoiture(Voiture $voiture): Response
+    {
+        if(!$voiture) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $id = $voiture->getId();
+
+        $this->entityManager->remove($voiture);
+
+
+        try {
+            $this->entityManager->flush();
+
+
+            $this->addFlash('success', "Supression de la voiture n°$id réussi. ");
+        }catch (Exception $message )
+        {
+            $this->addFlash('error', "Une erreur c'est produite lors de la suppression de la voiture n° $id.");
+        }
+
+
+        return $this->redirectToRoute('app_home');
     }
 }
